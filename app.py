db6706300ba0f951f959c2d07d9563cd22c8372b
luanamsilva <luanamoura2002@yahoo.com.br>
2024-03-05 10:14:26 -0300

    login_user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -10,6 +10,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -19,6 +24,8 @@ def login():
   if username and password:
     user = User.query.filter_by(username=username).first()
     if user and user.password == password:
+        login_user(user)
+        print(current_user.is_authenticated)
         return jsonify({"message": "Autenticação realizada com sucesso!"})
   return jsonify({"message": "Usuário ou senha inválidos"}), 400
 
